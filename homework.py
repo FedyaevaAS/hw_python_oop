90382e057c2257ae604e259e71aa312952acc6fe
Анастасия <fedyaevaAS@yandex.ru>
2021-08-19 22:44:26 +0300

    commit 7

--- homework.py
+++ homework.py
@@ -5,23 +5,21 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        self.today = dt.datetime.now().date()
 
     def add_record(self, record):
         self.records.append(record)
 
     def get_today_stats(self):
-        total = 0
-        for record in self.records:
-            if record.date == self.today:
-                total += record.amount
+        date = dt.date.today()
+        total = sum(record.amount for record in self.records 
+                if record.date == date)
         return total
 
     def get_week_stats(self):
-        total = 0
-        for record in self.records:
-            if self.today - dt.timedelta(days=7) <= record.date <= self.today:
-                total += record.amount
+        date = dt.date.today()
+        date_week_ago = date - dt.timedelta(days=7)
+        total = sum(record.amount for record in self.records
+                if date_week_ago <= record.date <= date)
         return total
 
     def get_remainder(self):
@@ -33,7 +31,7 @@ class Record:
     def __init__(self, amount, comment, date=None):
         date_format = '%d.%m.%Y'
         if date is None:
-            self.date = dt.datetime.now().date()
+            self.date = dt.date.today()
         else:
             self.date = dt.datetime.strptime(date, date_format).date()
         self.amount = amount
@@ -56,14 +54,17 @@ class CashCalculator(Calculator):
 
     def get_today_cash_remained(self, currency):
         self.currency = currency
-        currency_dict = {'rub': 'руб', 'usd': 'USD', 'eur': 'Euro'}
-        rate_dict = {'rub': 1, 'usd': self.USD_RATE, 'eur': self.EURO_RATE}
-        remainder = round(self.get_remainder() / rate_dict[self.currency], 2)
+        currency_dict = {'rub': ('руб', 1), 
+                         'usd': ('USD', self.USD_RATE), 
+                         'eur': ('Euro', self.EURO_RATE)}
+        currency_label, rate = currency_dict[currency]
+        remainder = round(self.get_remainder() / rate, 2)
+        abs_remainder = abs(remainder)
         if remainder > 0:
             return (f'На сегодня осталось {remainder} '
-                    f'{currency_dict[self.currency]}')
+                    f'{currency_label}')
         elif remainder < 0:
             return ('Денег нет, держись: твой долг - '
-                    f'{abs(remainder)} {currency_dict[self.currency]}')
+                    f'{abs_remainder} {currency_label}')
         else:
             return 'Денег нет, держись'
